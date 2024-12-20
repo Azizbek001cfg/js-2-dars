1467118ad1de6162169a9f5f3c54dfb024be9070
Azizbek <azizbek.aslonov2010@gmail.com>
2024-12-20 20:33:51 +0500

    1-ozgarish

---
+++ 2-dars.rar/scriptrar.js
@@ -0,0 +1,38 @@
+// 1. string => satrlardan iborat ma'lumot turi
+let ism = "Keldiyor"
+console.log(ism)
+console.log(typeof ism);
+
+// 2. number => sonlardan iborat ma'lumot turi
+let yosh = 21
+console.log(yosh);
+console.log(typeof yosh);
+
+// 3. boolean => mantiqiy ma'lumot turi. rost va yolg'on
+let toliqlilik = false
+console.log(toliqlilik);
+console.log(typeof toliqlilik);
+
+let dekabr = true
+console.log(typeof dekabr);
+
+// 4. undefined => hali aniqlanmagan qiymat
+let a
+console.log(typeof a);
+a = 31
+console.log(typeof a);
+
+// 5. null => bo'sh qiymatga ega bo'lgan ma'lumot turi
+let maktab = null
+console.log(typeof maktab);
+
+// typeof => ma'lumot turini aniqlaydigan operator.
+
+let x = 5
+let y = 6
+console.log(x>y); //false
+console.log(2*2==4); //true
+console.log(3*2!=6); //false
+console.log(4<=4); //true
+
+console.log(2*2==4 && 3>2);
