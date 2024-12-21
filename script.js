f97d251b30b96606e58e00f5caaeacce2f353e98
Azizbek <azizbek.aslonov2010@gmail.com>
2024-12-21 19:37:34 +0500

    2-chi

---
+++ script.js
@@ -0,0 +1,92 @@
+
+//  Number-sonlarni o'zida saqlovchi malumot turi
+let number = 21
+let son = 12
+let medium = 4
+let telephon = 3
+let large = 45
+
+console.log(number);
+console.log(son);
+console.log(medium);
+console.log(telephon);
+console.log(large);
+
+
+
+
+
+
+console.log(`_____________________________________________________________________`);
+console.log(`                          String`);
+
+
+// String-ozida belgilar ketma-ketligini saqlovchi ma'lumot turi yani harhlar
+
+let Firstname = `Azizbek`
+console.log(Firstname);
+let Lastname = `Aslonov`
+console.log(Lastname);
+let Drugsname = `Fozil`
+console.log(Drugsname);
+let Brathername = `Asadbek`
+console.log(Brathername);
+
+console.log (`_______________________________________________________________________________
+                            Bolean (true-false)`);
+
+// Bolean-mantiqiy  ma'lumot turibolib faqatgina true (rost) yoli false (yolgon) qiymatini 
+// qabul qilishi mumkin bolgan ma'lumot turi
+
+let Isname = `true`
+console.log(Isname);
+let Username = `false`
+console.log(Username);
+let LaStname = `true`
+console.log(LaStname);
+let FirstName = `False`
+console.log(FirstName);
+let isUser = `false`
+console.log(false);
+console.log(`__________________________________________________________________________
+                                Undefibed`);
+
+
+// Undefined-hali aniqlanmagan ma'lumot turi ekranga underfinet deb chiqadi 
+let student;
+console.log(student);
+let market;
+console.log(student);
+let book;
+console.log(book);
+let telephone;
+console.log(telephone);
+let ohirgisi;
+console.log(ohirgisi);
+
+console.log(`_______________________________________________________________________________
+                                Null`);
+
+
+// Null-bosh qiymatga ega bo'lgan ma'lumot turi
+
+let school = null
+console.log(school);
+let studeNt = null
+console.log(studeNt);
+let Student = null
+console.log(Student);
+let Large = null
+console.log(Large);
+let Medium = null
+console.log(Medium);
+
+
+
+
+
+
+
+
+
+
